67d54514a6b2c90ba463b134bdd19afb890fa70d
Dominik Dabrowski <dominik@doda.co>
2025-12-17 10:57:21 -0500

    Correct deploy with base paths

--- frontend/dist/_astro/channel.BMMYPXjN.js
+++
@@ -1,1 +0,0 @@
-import{ap as o,aq as n}from"./MermaidDiagram.astro_astro_type_script_index_0_lang.sEc_GWVt.js";const t=(a,r)=>o.lang.round(n.parse(a)[r]);export{t as c};
--- frontend/dist/_astro/clone.BRzgIMip.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./graph.BK33DEv3.js";var e=4;function a(o){return r(o,e)}export{a as c};
